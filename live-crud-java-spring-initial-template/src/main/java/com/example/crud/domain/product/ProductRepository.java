a1f79aefb7d62435498f96078df7fd98ffbf303a
Pietro-dev <pietro.pietromartins@gmail.com>
2025-03-15 17:22:01 -0300

    Crud finalizado

--- live-crud-java-spring-initial-template/src/main/java/com/example/crud/domain/product/ProductRepository.java
+++ live-crud-java-spring-initial-template/src/main/java/com/example/crud/domain/product/ProductRepository.java
@@ -1,4 +1,7 @@
 package com.example.crud.domain.product;
 
-public class ProductRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, String> {
+
 }
